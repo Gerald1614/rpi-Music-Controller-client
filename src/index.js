63e5b3c2955d0cf2e407e6548c557490a55ffa2c
gerald Michelant <gerald.michelant@gmail.com>
2019-09-09 22:21:59 -0400

    first version running on RPi with gpio

--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
-
+import Pigpio from 'pigpio'
 import { connect } from 'mqtt'
+import channels from './channels.json'
+
+const Gpio = Pigpio.Gpio
+const led = new Gpio(18, {mode: Gpio.OUTPUT});
+
 
 const client = connect('mqtt://mqtt');
 client.on('connect', () => {
-  const radio = { "action": "play", "channel": 0}
+  const radio = { "action": "play", "desc": channels[0].stream}
   client.publish('radio_action', JSON.stringify(radio))
   console.log(radio)
   
   setTimeout( async () => { 
-    const radio = { "action": "volume", "channel":0.2}
+    const radio = { "action": "volume", "desc":0.1}
     client.publish('radio_action', JSON.stringify(radio))
+    led.pwmWrite(255)
     console.log(radio)
   },15000)
 
   setTimeout( async () => { 
-    const radio = { "action": "volume", "channel":0.6}
+    const radio = { "action": "volume", "desc":0.2}
     client.publish('radio_action', JSON.stringify(radio))
     console.log(radio)
   },30000)
@@ -22,6 +28,7 @@ client.on('connect', () => {
   setTimeout( async () => { 
     const radio = { "action": "stop"}
     client.publish('radio_action', JSON.stringify(radio))
+    led.pwmWrite(0)
     console.log(radio)
   },45000)
 })
